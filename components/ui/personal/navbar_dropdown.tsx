546cf3ba5a04aef2c3ffa935e3a9d2da45ef13ab
MarkoPvlkvc <pavlakovic.marko@gmail.com>
2025-01-06 21:22:28 +0100

    fixed favicon not showing and mobile navbar bug

--- components/ui/personal/navbar_dropdown.tsx
+++ components/ui/personal/navbar_dropdown.tsx
@@ -8,14 +8,21 @@ import { useState } from "react";
 const NavbarDropdown = ({ className }: { className?: string }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const redirectTo = (path: string) => {
+    toggleMenu();
+    router.push(path);
+  };
+
   const router = useRouter();
 
   return (
     <div className={cn("relative z-20", className)}>
       <button
-        onClick={() => {
-          setIsOpen((prev) => !prev);
-        }}
+        onClick={toggleMenu}
         className="flex size-12 items-center justify-center rounded-full bg-darkBlue p-3 text-white transition-colors hover:bg-[#376075]"
       >
         <Menu className="" />
@@ -24,73 +31,55 @@ const NavbarDropdown = ({ className }: { className?: string }) => {
       {isOpen && (
         <div className="absolute right-0 top-14 flex w-max flex-col rounded-3xl border-[1px] border-black/15 bg-darkBlue pt-4 text-sm font-semibold text-white/60 focus:outline-none">
           <button
-            onClick={() => {
-              router.push("/");
-            }}
+            onClick={() => redirectTo("/")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             HOME
           </button>
           <button
-            onClick={() => {
-              router.push("/#about-us");
-            }}
+            onClick={() => redirectTo("/#about-us")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             ABOUT US
           </button>
           <button
-            onClick={() => {
-              router.push("/contact-&-info");
-            }}
+            onClick={() => redirectTo("/contact-&-info")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             CONTACT & INFO
           </button>
           <button
-            onClick={() => {
-              router.push("/faq");
-            }}
+            onClick={() => redirectTo("/faq")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             FAQ
           </button>
           <button
-            onClick={() => {
-              router.push("/kaze-no-yashiro");
-            }}
+            onClick={() => redirectTo("/kaze-no-yashiro")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             KAZE NO YASHIRO
           </button>
           <button
-            onClick={() => {
-              router.push("/yama-no-sato");
-            }}
+            onClick={() => redirectTo("/yama-no-sato")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             YAMA NO SATO
           </button>
           <button
-            onClick={() => {
-              router.push("/gallery");
-            }}
+            onClick={() => redirectTo("/gallery")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             GALLERY
           </button>
           <button
-            onClick={() => {
-              router.push("/offers");
-            }}
+            onClick={() => redirectTo("offers")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             OFFERS
           </button>
           <button
-            onClick={() => {
-              router.push("/news-&-events");
-            }}
+            onClick={() => redirectTo("/news-&-events")}
             className="w-full px-6 py-2 text-start transition-colors hover:text-white"
           >
             NEWS & EVENTS
@@ -98,9 +87,7 @@ const NavbarDropdown = ({ className }: { className?: string }) => {
 
           <div className="flex w-full items-center justify-center">
             <button
-              onClick={() => {
-                router.push("/book-now");
-              }}
+              onClick={() => redirectTo("/book-now")}
               className="m-6 flex items-center rounded-3xl bg-white px-6 py-5 text-lightBlue"
             >
               <Calendar className="mr-2 h-4 w-4" />
